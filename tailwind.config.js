c8d03ecb5fe077c007e628959f13ee3cf1957a99
JuanDEV <robertukisimon@hotmail.com>
2024-05-18 21:20:37 -0600

    FernandoStore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: ['./src/**/*.html', './src/**/*.js', './*.html'],
+  darkMode: 'media', // or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
