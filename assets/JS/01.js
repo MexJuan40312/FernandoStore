8728226c36f9970acb7d2464ab7e18981e5322c0
JuanDEV <robertukisimon@hotmail.com>
2024-05-18 20:43:05 -0600

    Sitio playeras

---
+++ assets/JS/01.js
@@ -0,0 +1,21 @@
+        // Script para el menú móvil
+        document.getElementById('mobile-menu-button').onclick = function () {
+            var menu = document.getElementById('mobile-menu');
+            if (menu.classList.contains('hidden')) {
+                menu.classList.remove('hidden');
+            } else {
+                menu.classList.add('hidden');
+            }
+        };
+
+        var swiper = new Swiper('.swiper-container', {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
